13809b8e492a24c8277587bfe3217bea1a3cf952
Pat-Lim <impatlim@gmail.com>
2020-04-17 12:45:41 +1200

    Complete quote generator

--- client/api/index.js
+++ client/api/index.js
@@ -1,35 +1,29 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: "https://api.chucknorris.io/jokes",
-  baseURL: "https://api.kanye.rest",
-  baseURL: "https://docs.tronalddump.io",
-  baseURL: "https://ron-swanson-quotes.herokuapp.com/v2"
-})
-
-
 // Random Chuck Norris Quote
 // JSON.value
 export const getChuckRandomJoke = () => {
-  return api.get("/random")
+  return axios.get("https://api.chucknorris.io/jokes/random")
+  .then(res => res.data.value )
 }
 
 // Random Kanye West Quote
 // JSON.quote
 export const getKanyeQuote = () => {
-  return api.get('/')
+  return axios.get('https://api.kanye.rest')
+  .then(res => res.data.quote)
 }
 
 // Random Donald Trump Quote
 // JSON.value
 export const getTonaldRandomQuote = () => {
-  return api.get('/random/quote')
+  return axios.get('https://tronalddump.io/random/quote')
+  .then(res => res.data.value)
 }
 
 // Random Ron Swanson Quote
 // JSON[0]
 export const getRonRandomQuote = () => {
-  return api.get('/quotes')
+  return axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+  .then(res => res.data[0])
 }
-
-export default api
--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from "react"
+import {
+  getChuckRandomJoke,
+  getKanyeQuote,
+  getTonaldRandomQuote,
+  getRonRandomQuote,
+} from "../api/index"
 
 class Game extends Component {
   state = {
@@ -6,38 +12,123 @@ class Game extends Component {
     rightScore: 0,
     leftQuote: "",
     rightQuote: "",
-    leftImg: "",
-    rightImg: "",
+    leftCharacter: "chuck",
+    rightCharacter: "chuck",
     quotesGenerated: false,
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.setState({ leftQuote: "loading...", rightQuote: "loading..." })
+    switch (this.state.leftCharacter) {
+      case "chuck":
+        getChuckRandomJoke().then((res) => {
+          this.setState({
+            leftQuote: res,
+          })
+        })
+        break
+      case "kanye":
+        getKanyeQuote().then((res) => {
+          this.setState({
+            leftQuote: res,
+          })
+        })
+        break
+      case "donald":
+        getTonaldRandomQuote().then((res) => {
+          this.setState({
+            leftQuote: res,
+          })
+        })
+        break
+      case "ron":
+        getRonRandomQuote().then((res) => {
+          this.setState({
+            leftQuote: res,
+          })
+        })
+        break
+    }
+    switch (this.state.rightCharacter) {
+      case "chuck":
+        getChuckRandomJoke().then((res) => {
+          this.setState({
+            rightQuote: res,
+          })
+        })
+        break
+      case "kanye":
+        getKanyeQuote().then((res) => {
+          this.setState({
+            rightQuote: res,
+          })
+        })
+        break
+      case "donald":
+        getTonaldRandomQuote().then((res) => {
+          this.setState({
+            rightQuote: res,
+          })
+        })
+        break
+      case "ron":
+        getRonRandomQuote().then((res) => {
+          this.setState({
+            rightQuote: res,
+          })
+        })
+        break
+    }
+    this.setState({
+      quotesGenerated: true,
+    })
+  }
 
-  // handleLeftClick () event => {
-  //   if(this.state.quotesGenerated){
-  //     return
-  //   }
-  //   this.setState({
-  //       leftScore: event.target.value
-  //     })
-  // }
+  handleLeftClick = () => {
+    this.state.quotesGenerated &&
+      this.setState({
+        leftScore: this.state.leftScore + 1,
+        quotesGenerated: false,
+      })
+  }
 
-  // handleRightClick () event => {
-  //   if(this.state.quotesGenerated){
-  //     return
-  //   }
-  //   this.setState({
-  //       rightScore: event.target.value
-  //     })
-  // }
+  handleRightClick = () => {
+    this.state.quotesGenerated &&
+      this.setState({
+        rightScore: this.state.rightScore + 1,
+        quotesGenerated: false,
+      })
+  }
+
+  handleLeftChange = (event) => {
+    this.setState({
+      leftCharacter: event.target.value,
+    })
+  }
+
+  handleRightChange = (event) => {
+    this.setState({
+      rightCharacter: event.target.value,
+    })
+  }
+
+  resetGame = (event) => {
+    this.setState({
+      leftScore: 0,
+      rightScore: 0,
+      leftQuote: "",
+      rightQuote: "",
+      quotesGenerated: false,
+    })
+  }
 
   render() {
     return (
       <>
         <div className="leftColumn">
           <div className="leftInput">
-            <select name="leftCharacter">
-              <option value="chuck">Chuck</option>
+            <select name="leftCharacter" onChange={this.handleLeftChange}>
+              <option value="chuck">Chuck Norris</option>
               <option value="ron">Ron Swanson</option>
               <option value="kanye">Kanye West</option>
               <option value="donald">Donald Trump</option>
@@ -46,24 +137,33 @@ class Game extends Component {
           </div>
           <div className="speechBubble" onClick={this.handleLeftClick}>
             <img alt="speech bubble" />
-            <p>speech bubble</p>
+            <p>{this.state.leftQuote}</p>
           </div>
-          <img alt="Left character image" />
-          <button>Generate Quote</button>
+          <img
+            src={`left${this.state.leftCharacter}.png`}
+            alt="Left character image"
+          />
         </div>
+
+        <button onClick={() => this.componentDidMount()}>Generate Quote</button>
+        <button onClick={this.resetGame}>Reset Game</button>
+
         <div className="rightColumn">
           <div className="rightInput">
-            <select name="rightCharacter">
+            <select name="rightCharacter" onChange={this.handleRightChange}>
               <option value="chuck">Chuck Norris</option>
               <option value="ron">Ron Swanson</option>
               <option value="kanye">Kanye West</option>
               <option value="donald">Donald Trump</option>
             </select>
-            <h1>Score: {this.state.leftScore}</h1>
+            <h1>Score: {this.state.rightScore}</h1>
           </div>
-          <div className="speechBubble" onClick={this.rightClickHandler}>
-            <img alt="speech bubble" />
-            <p>speech bubble</p>
+          <div className="speechBubble" onClick={this.handleRightClick}>
+            <img
+              src={`right${this.state.rightCharacter}.png`}
+              alt="right character image"
+            />
+            <p>{this.state.rightQuote}</p>
           </div>
           <img alt="Right character image" />
         </div>
